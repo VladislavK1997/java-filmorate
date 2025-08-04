62da7217063cef5486cf650d727a87dcb0f1b385
VladK <vlad.katrich1997@yandex.ru>
2025-08-04 16:03:30 +0300

    Исправлено

---
+++ src/main/java/ru/yandex/practicum/filmorate/exception/GlobalExceptionHandler.java
@@ -0,0 +1,41 @@
+package ru.yandex.practicum.filmorate.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<Map<String, String>> handleUserNotFound(ValidationException ex) {
+        Map<String, String> error = Map.of(
+                "timestamp", LocalDateTime.now().toString(),
+                "status", "404",
+                "error", "User Not Found",
+                "message", ex.getMessage()
+        );
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
+
+        return new ResponseEntity<>(Map.of("error", "Validation failed"), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, String>> handleOtherExceptions(Exception ex) {
+        return new ResponseEntity<>(Map.of(
+                "timestamp", LocalDateTime.now().toString(),
+                "status", "500",
+                "error", "Internal Server Error",
+                "message", ex.getMessage()
+        ), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
