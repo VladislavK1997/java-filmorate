858f6050b87665ca8124b2e87b8f8ecb7a74f4f4
VladK <vlad.katrich1997@yandex.ru>
2025-08-14 11:50:39 +0300

    Resolved merge conflicts between add-friends-likes and controllers-films-users

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,21 +1,31 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
+import jakarta.validation.ValidationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
 
+import java.time.LocalDate;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
 @RequestMapping("/films")
+@Validated
 public class FilmController {
 
     private final FilmService filmService;
+    private final Map<Integer, Film> films = new HashMap<>();
+    private int currentId = 1;
+    private static final LocalDate MIN_RELEASE_DATE = LocalDate.of(1895, 12, 28);
 
     @Autowired
     public FilmController(FilmService filmService) {
@@ -24,11 +34,28 @@ public class FilmController {
 
     @PostMapping
     public Film create(@Valid @RequestBody Film film) {
+        if (film.getReleaseDate().isBefore(MIN_RELEASE_DATE)) {
+            throw new ValidationException("Дата релиза не может быть раньше 28.12.1895");
+        }
+
+        film.setId(currentId++);
+        films.put(film.getId(), film);
+        log.info("Добавлен фильм: {}", film);
         return filmService.create(film);
     }
 
     @PutMapping
     public Film update(@Valid @RequestBody Film film) {
+        if (!exists(film.getId())) {
+            throw new NotFoundException("Фильм не найден");
+        }
+
+        if (film.getReleaseDate().isBefore(MIN_RELEASE_DATE)) {
+            throw new ValidationException("Дата релиза не может быть раньше 28.12.1895");
+        }
+
+        films.put(film.getId(), film);
+        log.info("Обновлен фильм: {}", film);
         return filmService.update(film);
     }
 
@@ -55,5 +82,9 @@ public class FilmController {
     @GetMapping("/popular")
     public List<Film> getPopular(@RequestParam(name = "count", defaultValue = "10") int count) {
         return filmService.getPopular(count);
+    }
+
+    private boolean exists(int id) {
+        return films.containsKey(id);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -3,19 +3,25 @@ package ru.yandex.practicum.filmorate.controller;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
 
 import java.util.Collection;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestController
 @RequestMapping("/users")
+@Validated
 public class UserController {
 
     private final UserService userService;
+    private final Map<Integer, User> users = new HashMap<>();
+    private int currentId = 1;
 
     @Autowired
     public UserController(UserService userService) {
@@ -24,11 +30,19 @@ public class UserController {
 
     @PostMapping
     public User create(@Valid @RequestBody User user) {
+        user.setId(currentId++);
+        users.put(user.getId(), user);
+        log.info("Добавлен пользователь: {}", user);
         return userService.create(user);
     }
 
     @PutMapping
     public User update(@Valid @RequestBody User user) {
+        if (!exists(user.getId())) {
+            throw new NotFoundException("Пользователь не найден");
+        }
+        users.put(user.getId(), user);
+        log.info("Обновлен пользователь: {}", user);
         return userService.update(user);
     }
 
@@ -53,13 +67,18 @@ public class UserController {
     }
 
     @GetMapping("/{id}/friends")
-    public List<User> getFriends(@PathVariable int id) {
+    public Collection<User> getFriends(@PathVariable int id) {
         return userService.getFriends(id);
     }
 
     @GetMapping("/{id}/friends/common/{otherId}")
-    public List<User> getCommonFriends(@PathVariable int id, @PathVariable int otherId) {
+    public Collection<User> getCommonFriends(@PathVariable int id, @PathVariable int otherId) {
         return userService.getCommonFriends(id, otherId);
     }
+
+    private boolean exists(int id) {
+        return users.containsKey(id);
+    }
 }
+
 
