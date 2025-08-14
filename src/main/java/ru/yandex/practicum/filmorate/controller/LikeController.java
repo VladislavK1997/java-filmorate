20652be13432391e3e547ff0752af0eec11c2be1
VladK <vlad.katrich1997@yandex.ru>
2025-08-14 14:02:59 +0300

    Исправлено

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/LikeController.java
@@ -0,0 +1,47 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/films")
+@RequiredArgsConstructor
+public class LikeController {
+
+    private final FilmService filmService;
+
+    @PutMapping("/{id}/like/{userId}")
+    public void addLike(@PathVariable int id, @PathVariable int userId) {
+        if (!filmService.exists(id)) {
+            throw new IllegalArgumentException("Фильм не найден: id=" + id);
+        }
+        if (!filmService.exists(userId)) {
+            throw new IllegalArgumentException("Пользователь не найден: id=" + userId);
+        }
+        filmService.addLike(id, userId);
+        log.info("Лайк добавлен: пользователь {} -> фильм {}", userId, id);
+    }
+
+    @DeleteMapping("/{id}/like/{userId}")
+    public void removeLike(@PathVariable int id, @PathVariable int userId) {
+        if (!filmService.exists(id)) {
+            throw new IllegalArgumentException("Фильм не найден: id=" + id);
+        }
+        if (!filmService.exists(userId)) {
+            throw new IllegalArgumentException("Пользователь не найден: id=" + userId);
+        }
+        filmService.removeLike(id, userId);
+        log.info("Лайк удалён: пользователь {} -> фильм {}", userId, id);
+    }
+
+    @GetMapping("/popular")
+    public List<Film> getPopular(@RequestParam(defaultValue = "10") int count) {
+        return filmService.getPopular(count);
+    }
+}
