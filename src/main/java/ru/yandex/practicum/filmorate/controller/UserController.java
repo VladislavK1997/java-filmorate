c98c0ae6b48c6cd949bac797677b9195f3192209
VladK <vlad.katrich1997@yandex.ru>
2025-08-14 12:42:52 +0300

    проект 11

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -62,4 +62,3 @@ public class UserController {
         return userService.getCommonFriends(id, otherId);
     }
 }
-
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -31,4 +31,3 @@ public class Film {
 
     private Set<Integer> likes = new HashSet<>();
 }
-
