6a1538b1515f6135bcd48eaa1c37e20c5d1783d2
VladK <vlad.katrich1997@yandex.ru>
2025-08-14 14:06:04 +0300

    Исправлено

--- src/main/java/ru/yandex/practicum/filmorate/controller/LikeController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/LikeController.java
@@ -3,8 +3,10 @@ package ru.yandex.practicum.filmorate.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
+import ru.yandex.practicum.filmorate.service.UserService;
 
 import java.util.List;
 
@@ -15,14 +17,15 @@ import java.util.List;
 public class LikeController {
 
     private final FilmService filmService;
+    private final UserService userService;
 
     @PutMapping("/{id}/like/{userId}")
     public void addLike(@PathVariable int id, @PathVariable int userId) {
         if (!filmService.exists(id)) {
-            throw new IllegalArgumentException("Фильм не найден: id=" + id);
+            throw new NotFoundException("Фильм не найден: id=" + id);
         }
-        if (!filmService.exists(userId)) {
-            throw new IllegalArgumentException("Пользователь не найден: id=" + userId);
+        if (!userService.exists(userId)) {
+            throw new NotFoundException("Пользователь не найден: id=" + userId);
         }
         filmService.addLike(id, userId);
         log.info("Лайк добавлен: пользователь {} -> фильм {}", userId, id);
@@ -31,10 +34,10 @@ public class LikeController {
     @DeleteMapping("/{id}/like/{userId}")
     public void removeLike(@PathVariable int id, @PathVariable int userId) {
         if (!filmService.exists(id)) {
-            throw new IllegalArgumentException("Фильм не найден: id=" + id);
+            throw new NotFoundException("Фильм не найден: id=" + id);
         }
-        if (!filmService.exists(userId)) {
-            throw new IllegalArgumentException("Пользователь не найден: id=" + userId);
+        if (!userService.exists(userId)) {
+            throw new NotFoundException("Пользователь не найден: id=" + userId);
         }
         filmService.removeLike(id, userId);
         log.info("Лайк удалён: пользователь {} -> фильм {}", userId, id);
