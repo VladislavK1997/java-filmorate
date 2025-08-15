032096872689086fd100d5c9ea8816dfa1e1f1e9
VladK <vlad.katrich1997@yandex.ru>
2025-08-15 13:29:11 +0300

    Исправлено

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,44 +1,67 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
 
+import java.time.LocalDate;
+import java.util.Collection;
 import java.util.List;
 
 @RestController
 @RequestMapping("/films")
+@RequiredArgsConstructor
 public class FilmController {
 
     private final FilmService filmService;
 
-    public FilmController(FilmService filmService) {
-        this.filmService = filmService;
-    }
-
-    @GetMapping
-    public List<Film> getAllFilms() {
-        return filmService.getAll();
-    }
-
-    @GetMapping("/{id}")
-    public Film getFilm(@PathVariable Long id) {
-        return filmService.getById(id);
+    private void checkFilmExists(long id) {
+        if (!filmService.exists(id)) {
+            throw new FilmNotFoundException("Film with id " + id + " not found");
+        }
     }
 
     @PostMapping
-    public Film addFilm(@RequestBody Film film) {
+    public Film create(@RequestBody Film film) {
+        validateFilm(film);
         return filmService.add(film);
     }
 
     @PutMapping
-    public Film updateFilm(@RequestBody Film film) {
+    public Film update(@RequestBody Film film) {
+        checkFilmExists(film.getId());
+        validateFilm(film);
         return filmService.update(film);
     }
 
-    @DeleteMapping("/{id}")
-    public void removeFilm(@PathVariable Long id) {
-        filmService.remove(id);
+    @GetMapping
+    public Collection<Film> getAll() {
+        return filmService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Film getById(@PathVariable long id) {
+        checkFilmExists(id);
+        return filmService.getById(id);
+    }
+
+    private void validateFilm(Film film) {
+        if (film.getName() == null || film.getName().isBlank()) {
+            throw new ValidationException("Film name cannot be empty");
+        }
+        if (film.getDescription() != null && film.getDescription().length() > 200) {
+            throw new ValidationException("Description cannot be longer than 200 characters");
+        }
+        if (film.getReleaseDate() != null && film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            throw new ValidationException("Release date cannot be before 28.12.1895");
+        }
+        if (film.getDuration() <= 0) {
+            throw new ValidationException("Duration must be positive");
+        }
     }
 }
+
 
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,44 +1,68 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
 
+import java.time.LocalDate;
+import java.util.Collection;
 import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
-    @GetMapping
-    public List<User> getAllUsers() {
-        return userService.getAll();
-    }
-
-    @GetMapping("/{id}")
-    public User getUser(@PathVariable Long id) {
-        return userService.getById(id);
+    // Проверка существования пользователя
+    private void checkUserExists(long id) {
+        if (!userService.exists(id)) {
+            throw new UserNotFoundException("User with id " + id + " not found");
+        }
     }
 
     @PostMapping
-    public User addUser(@RequestBody User user) {
+    public User create(@RequestBody User user) {
+        validateUser(user);
         return userService.add(user);
     }
 
     @PutMapping
-    public User updateUser(@RequestBody User user) {
+    public User update(@RequestBody User user) {
+        checkUserExists(user.getId());
+        validateUser(user);
         return userService.update(user);
     }
 
-    @DeleteMapping("/{id}")
-    public void removeUser(@PathVariable Long id) {
-        userService.remove(id);
+    @GetMapping
+    public Collection<User> getAll() {
+        return userService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getById(@PathVariable long id) {
+        checkUserExists(id);
+        return userService.getById(id);
+    }
+
+    private void validateUser(User user) {
+        if (user.getEmail() == null || !user.getEmail().contains("@")) {
+            throw new ValidationException("Email is invalid");
+        }
+        if (user.getLogin() == null || user.getLogin().isBlank() || user.getLogin().contains(" ")) {
+            throw new ValidationException("Login is invalid");
+        }
+        if (user.getBirthday() != null && user.getBirthday().isAfter(LocalDate.now())) {
+            throw new ValidationException("Birthday cannot be in the future");
+        }
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin()); // default name = login
+        }
     }
 }
+
 
